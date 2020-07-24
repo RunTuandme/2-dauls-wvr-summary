7956335ade4a2c9edf80f174faac9ec7b5931f4b
ZL <649553297@qq.com>
2020-07-24 08:33:52 +0800

    完成compare_bn.py中对Lv的并行计算，时间30分钟，可继续优化

--- compare_bn.py
+++ compare_bn.py
@@ -80,6 +80,8 @@ for file in filelist:
     plt.savefig('prod_vs_cal/' + date + '.png')
     plt.close('all')  """
 
+# --------------- 单个文件读取 --------------- #
+
 import sys
 sys.path.append('d:/work/two_daul/WVR')
 
@@ -89,67 +91,84 @@ from WVR.e0 import e0
 from WVR.tau_ import tau
 import matplotlib.pyplot as plt
 import os
+from multiprocessing import Pool
 
-def t2s(t):
+
+def t2s(t: str):
     h,m,s = t.strip().split(":")
     return float(h) * 3600 + float(m) * 60 + float(s)
 
-WVR_time = []
-WVR_ZWD = []
-WVR_Tb1 = []
-WVR_Tb2 = []
-WVR_T0 = []
-WVR_P0 = []
-WVR_e = []      # 相对湿度
+def Mutiwork(Team: tuple) -> float:
+    lv = Lv(Team[0], Team[1], 23.8, 31.2, Team[2], Team[3], Team[4], 0.0492)
+    return lv[3]
 
-file = 'raw_data/20181101/SH_FSJ_D1811010004.txt'
-with open(file,'r') as f_wvr:
-    read = False
-    for line in f_wvr:
-        if line in ['\n','\r\n']:
+if __name__ == '__main__':
+
+    WVR_time = []
+    WVR_ZWD = []
+    WVR_Tb1 = []
+    WVR_Tb2 = []
+    WVR_T0 = []
+    WVR_P0 = []
+    WVR_e = []      # 相对湿度
+
+    file = 'raw_data/20181101/SH_FSJ_D1811010004.txt'
+    with open(file,'r') as f_wvr:
+        read = False
+        for line in f_wvr:
+            if line in ['\n','\r\n']:
+                pass
+            else:
+                Arr = line.strip().split()
+                if len(Arr[0]) > 4:
+                    if ':' in Arr[3]:
+                        del Arr[:2]
+                    if Arr[0][:3] == '201':
+                        WVR_time.append(t2s(Arr[1]))
+                        WVR_ZWD.append(float(Arr[3]))
+                        WVR_Tb1.append(float(Arr[7]))
+                        WVR_Tb2.append(float(Arr[8]))
+                        WVR_T0.append(float(Arr[9])+273.15)
+                        WVR_e.append(float(Arr[10])/100)
+                        WVR_P0.append(float(Arr[11]))
+                        read = True
+        if not read:
             pass
-        else:
-            Arr = line.strip().split()
-            if len(Arr[0]) > 4:
-                if ':' in Arr[3]:
-                    del Arr[:2]
-                if Arr[0][:3] == '201':
-                    WVR_time.append(t2s(Arr[1]))
-                    WVR_ZWD.append(float(Arr[3]))
-                    WVR_Tb1.append(float(Arr[7]))
-                    WVR_Tb2.append(float(Arr[8]))
-                    WVR_T0.append(float(Arr[9])+273.15)
-                    WVR_e.append(float(Arr[10])/100)
-                    WVR_P0.append(float(Arr[11]))
-                    read = True
-    if not read:
-        pass
-f_wvr.close()
+    f_wvr.close()
 
-WVR_tau1 = [tau(a, b) for a,b in zip(WVR_Tb1, WVR_T0)]
-WVR_tau2 = [tau(a, b) for a,b in zip(WVR_Tb2, WVR_T0)]
+    WVR_tau1 = [tau(a, b) for a,b in zip(WVR_Tb1, WVR_T0)]
+    WVR_tau2 = [tau(a, b) for a,b in zip(WVR_Tb2, WVR_T0)]
 
-pv = [e0(T0)*e for T0, e in zip(WVR_T0, WVR_e)]
+    pv = [e0(T0)*e for T0, e in zip(WVR_T0, WVR_e)]
 
-rebuild = list(zip(WVR_Tb1, WVR_Tb2, pv, WVR_T0, WVR_P0))
+    rebuild = list(zip(WVR_Tb1, WVR_Tb2, pv, WVR_T0, WVR_P0))
 
-#WVR_Lv = [Lv(TB1, TB2, 23.8, 31.2, p_v, T0, P0, 0.0492)[3] for TB1, TB2, p_v, T0, P0 in zip(WVR_Tb1, WVR_Tb2, pv, WVR_T0, WVR_P0)]
+    p = Pool(6)
+    pbar = tqdm(total=len(rebuild))
+    def Update(*a):
+        pbar.update()
 
-WVR_Lv = []
+    ms = []
+    for i in rebuild:
+        result = p.apply_async(Mutiwork, args=(i,), callback=Update)
+        ms.append(result)
 
-bar = tqdm(rebuild)
-for i in bar:
-    lv = Lv(i[0], i[1], 23.8, 31.2, i[2], i[3], i[4], 0.0492)
-    WVR_Lv.append(lv[3])
+    p.close()
+    p.join()
 
-plt.plot(WVR_time,WVR_Lv,label='WVR_calculates')
-plt.plot(WVR_time,WVR_ZWD,label='WVR_products')
-plt.title('products-calculates date:181101')
-plt.ylabel('Zenith wet path delay(mm)')
-plt.xlabel('time(s)')
-plt.legend()
-plt.savefig('prod_vs_cal/181101.png') 
+    WVR_Lv = []
+    for i in ms:
+        WVR_Lv.append(i.get())   # 注意：获取返回值get()方法一定要在进程池回收之后进行，否则进程阻塞。无法并行。
 
-with open('prod_vs_cal/181101.txt','w') as f_out:
-    f_out.writelines(WVR_Lv)
-f_out.close()
+    plt.plot(WVR_time,WVR_Lv,label='WVR_calculates')
+    plt.plot(WVR_time,WVR_ZWD,label='WVR_products')
+    plt.title('products-calculates date:181101')
+    plt.ylabel('Zenith wet path delay(mm)')
+    plt.xlabel('time(s)')
+    plt.legend()
+    plt.savefig('prod_vs_cal/181101.png') 
+
+    with open('prod_vs_cal/181101.txt','w') as f_out:
+        for i in WVR_Lv:
+            f_out.write(str(i) + '\n')
+    f_out.close()
